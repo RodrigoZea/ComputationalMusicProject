93644e9ebd99775f311bee0cb218e1ccef37cd58
RodrigoZea <zearodrigo37@gmail.com>
2021-07-21 16:34:01 -0600

    Adding metronome functionality with and without subdivision, no doc or display yet.

---
+++ MusicProject/Assets/Scripts/MainMetronome.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMetronome : MonoBehaviour
+{
+    // AudioClip 1: First metronome indicator
+    // AudioClip 2: Rest of indicators
+    public List<AudioClip> audioClips; 
+   private AudioSource audioSource; 
+   public InputField bpm;
+   public Slider metric; 
+   public Toggle subdivisionMode;
+
+   private int counter;
+   private float interval;
+   private float beatsPerBar;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        beatsPerBar = metric.value;
+        interval = 60.0f/(float.Parse(bpm.text));
+        Debug.Log("Interval is: " + interval);
+
+        Debug.Log("subdivision:" + subdivisionMode.isOn);
+
+
+        if (subdivisionMode.isOn) {
+            StartCoroutine("metronomeSubdivided");
+        } else {
+            StartCoroutine("metronome");
+        }
+
+        
+    }
+
+    IEnumerator metronome() {
+        counter = 0;
+        while (true) {
+            counter++;
+            if (counter % beatsPerBar == 1) {
+                audioSource.PlayOneShot(audioClips[0], 0.7F);
+                Debug.Log("start beat");
+            } else {
+                audioSource.PlayOneShot(audioClips[1], 0.7F);
+                Debug.Log("other beat");
+            }
+
+            yield return new WaitForSecondsRealtime(interval);
+        }
+    }
+
+    IEnumerator metronomeSubdivided() {
+        counter = 0;
+        beatsPerBar = beatsPerBar*2;
+        while (true) {
+            counter++;
+            if (counter % beatsPerBar == 1) {
+                audioSource.PlayOneShot(audioClips[0], 0.7F);
+                Debug.Log("Beat inicial");
+            } else {
+                if (counter % 2 == 0){
+                    audioSource.PlayOneShot(audioClips[2], 0.7F);
+                    Debug.Log("Beat subdividido");
+                } else {
+                    audioSource.PlayOneShot(audioClips[1], 0.7F);
+                    Debug.Log("Beat entero");
+                }
+            }
+            
+            yield return new WaitForSecondsRealtime(interval/2);
+        }
+    }
+
+    void RenderCircles(){
+
+    }
+}
