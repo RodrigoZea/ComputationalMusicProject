d4d6001fe38dead53c35bcff4f43299e5a392a3e
RodrigoZea <zearodrigo37@gmail.com>
2021-08-11 19:09:01 -0600

    generating keys with arrays

--- MusicProject/Assets/Scripts/ProceduralMusicRelated/ProceduralMusicGenerator.cs
+++ MusicProject/Assets/Scripts/ProceduralMusicRelated/ProceduralMusicGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ProceduralMusicGenerator : MonoBehaviour
@@ -12,7 +13,9 @@ public class ProceduralMusicGenerator : MonoBehaviour
 
     void Start()
     {
-        generateRhythm(0);
+        int[] metric = generateRhythm(0);
+        int[] generatedKey = generateKey(metric);
+        //generateFiller(generatedKey, metric);
     }
 
     // Update is called once per frame
@@ -21,7 +24,7 @@ public class ProceduralMusicGenerator : MonoBehaviour
         
     }
 
-    void generateRhythm(int seed) {
+    int[] generateRhythm(int seed) {
         int initialSeed;
         List<int[]> metrics = new List<int[]>();
 
@@ -44,11 +47,13 @@ public class ProceduralMusicGenerator : MonoBehaviour
         int metricSelection = Random.Range(0, metrics.Count);
         int[] selectedMetric = metrics[metricSelection];
 
-        generateKey(selectedMetric);
+        return selectedMetric;
     }
 
-    void generateKey(int[] selectedMetric) {
+    int[] generateKey(int[] selectedMetric) {
+        // Can be 3 or 4
         int cantidadSubdivision  = selectedMetric[0];
+        // Is always 4.
         int subdivisionBase = selectedMetric[1];
 
         // For the beat array
@@ -64,12 +69,44 @@ public class ProceduralMusicGenerator : MonoBehaviour
         int[] possibleSubdivisions = {1, 2, 4};
         // Pick a random subdivision
         int randomSubdivision = Random.Range(0, possibleSubdivisions.Length);
-        int selectedSubdivisionMultiplier = possibleSubdivisions[randomSubdivision];
-        // Can be either 4, 8 or 16 depending on the what the random picks.
-        int newBaseSub = subdivisionBase*selectedSubdivisionMultiplier;
+        // Can be either 3, 6, 12 / 4, 8, 16 depending on the what the random picks.
+        int newSubdivisionCount = cantidadSubdivision*possibleSubdivisions[randomSubdivision];
+
+        // ----------------------------------------------------------------------------
+        // Possible values 
+        int[] values = {2,3};
+   
+        int currentKeyPos = 0;
+        Debug.Log("Array Size: " + keysSize);
+        Debug.Log("Limit: " + newSubdivisionCount);
+        // Algorithm: Pick random numbers and append, validate on each append that the sum of the values is not bigger than the size allowed.
+        while(keysArray.Sum() < newSubdivisionCount) {
+            // Pick random number. Either 2 or 3.
+            int rand = values[Random.Range(0, values.Length)];
+            // If value to append is not bigger than the size allowed, add it to array.
+            // i.e. limit is 12 keys and our current array looks like [2, 2, 2, 2, 2, x]
+            //      sum is 10 and will only accept a new value of 2, a value of 3 is not possible to append.
+            if (rand + keysArray.Sum() <= newSubdivisionCount) {  
+                keysArray[currentKeyPos] = rand;
+                // On successful append...
+                currentKeyPos += 1;
+            } 
+        }
+
+        Debug.Log("Clave test: " + string.Join(",", keysArray));
+
+        return keysArray;
     }
 
-    void generateFiller() {
+    void generateFiller(int[] keys, int[] metric) {
+        List<int> result = new List<int>();
+        
+        foreach(int note in keys) {
+            if (note == 2) result.AddRange(new List<int>{0, 1, 1});
+            else if (note == 3) result.AddRange(new List<int>{0, 1});
+        }
 
+        int[] fillingResult = result.ToArray();
+        Debug.Log("Filling test: " + string.Join(",", fillingResult));
     }
 }
